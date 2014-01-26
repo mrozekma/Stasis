ecfa169e4562fe4b158d6430cb3c7ce7f2a0c1ee
Michael Mrozek <git@mrozekma.com>
2014-01-26 04:40:49 -0500

    Support non-int keys, and store the next ID instead of trying to derive it

--- DiskMap.py
+++ DiskMap.py
@@ -1,8 +1,14 @@
 import jsonpickle
 from os import listdir, mkdir, remove
-from os.path import isfile, isdir, join as pathjoin
+from os.path import exists, isfile, isdir, join as pathjoin
 
 from StasisError import StasisError
+
+def to_int(n, allowAny = True):
+	try:
+		return int(n)
+	except ValueError:
+		return n if allowAny else None
 
 class DiskMap:
 	def __init__(self, dir, create = False, cache = False):
@@ -30,6 +36,9 @@ class DiskMap:
 	def __len__(self):
 		return len(listdir(self.dir))
 
+	def __contains__(self, key):
+		return exists(pathjoin(self.dir, str(key)))
+
 	def __iter__(self):
 		for key in listdir(self.dir):
 			yield key
@@ -49,6 +58,8 @@ class TableMap:
 			mkdir(self.dir)
 		with open(pathjoin(self.dir, str(key)), 'w') as f:
 			f.write(jsonpickle.encode(value))
+		if isinstance(key, int) and key >= self.nextID():
+			self['__nextid'] = key + 1
 
 	def __delitem__(self, key):
 		path = pathjoin(self.dir, str(key))
@@ -56,11 +67,17 @@ class TableMap:
 			remove(path)
 
 	def __len__(self):
-		return len(listdir(self.dir))
+		return len(self.keys())
+
+	def __contains__(self, key):
+		return exists(pathjoin(self.dir, str(key)))
 
 	def __iter__(self):
-		for key in listdir(self.dir):
+		for key in self.keys():
 			yield key
+
+	def keys(self):
+		return filter(lambda key: not (isinstance(key, str) and key.startswith('__')), listdir(self.dir))
 
 	def change(self, key):
 		return PendingChange(self, key)
@@ -68,50 +85,51 @@ class TableMap:
 	def all(self):
 		if not isdir(self.dir):
 			return {}
-		ids = sorted(map(int, listdir(self.dir)))
+		ids = map(to_int, self.keys())
 		return {id: self[id] for id in ids}
 
 	def nextID(self):
-		if not isdir(self.dir):
-			return 1
-		ids = map(int, listdir(self.dir))
-		if ids == []:
-			return 1
-		return max(ids) + 1
+		return self['__nextid'] if '__nextid' in self else 1
 
 class CachedTableMap:
 	def __init__(self, diskMap, path):
 		self.diskMap = diskMap
 		self.dir = path
-		self.cache = TableMap(self.dir).all()
+		self.parent = TableMap(self.dir)
+		self.cache = self.parent.all()
 
 	def __getitem__(self, key):
 		return self.cache[key]
 
 	def __setitem__(self, key, value):
 		self.cache[key] = value
-		TableMap(self.dir)[key] = value
+		self.parent[key] = value
 
 	def __delitem__(self, key):
 		del self.cache[key]
-		del TableMap(self.dir)[key]
+		del self.parent[key]
 
 	def __len__(self):
-		return len(self.cache)
+		return len(self.keys())
+
+	def __contains__(self, key):
+		return key in self.cache
 
 	def __iter__(self):
-		return self.cache.__iter__()
+		for key in self.keys():
+			yield key
+
+	def keys(self):
+		return filter(lambda key: not (isinstance(key, str) and key.startswith('__')), self.cache)
 
 	def change(self, key):
 		return PendingChange(self, key)
 
 	def all(self):
-		return self.cache
+		return {key: self[key] for key in self.keys()}
 
 	def nextID(self):
-		if self.cache == {}:
-			return 1
-		return max(self.cache) + 1
+		return self.parent.nextID()
 
 class PendingChange:
 	def __init__(self, diskMap, key):
