8f04895939eb7d7cc74604a9cdf7ddc0053c92af
Michael Mrozek <git@mrozekma.com>
2014-01-27 01:49:01 -0500

    Merged Link into ActiveRecord

--- ActiveRecord.py
+++ ActiveRecord.py
@@ -45,3 +45,39 @@ class ActiveRecord(object):
 		cls = self.__class__
 		if self.id:
 			del store()[cls.table()][self.id]
+
+# getter
+def idToObj(cls, field):
+	def fn(self):
+		if not self:
+			return None
+		val = getattr(self, field)
+		if isinstance(val, list):
+			return map(cls.load, val)
+		elif isinstance(val, set):
+			return set(map(cls.load, val))
+		else:
+			return cls.load(val)
+	return fn
+
+# setter
+def objToID(field):
+	def fn(self, obj):
+		if isinstance(obj, list):
+			if not all(o.id for o in obj):
+				raise ValueError("Attempted to pull ID from unsaved object in list")
+			setattr(self, field, [o.id for o in obj])
+		elif isinstance(obj, set):
+			if not all(o.id for o in obj):
+				raise ValueError("Attempted to pull ID from unsaved object in set")
+			setattr(self, field, set(o.id for o in obj))
+		elif obj:
+			if not obj.id:
+				raise ValueError("Attempted to pull ID from unsaved object")
+			setattr(self, field, obj.id)
+		else:
+			setattr(self, field, 0)
+	return fn
+
+def link(cls, field):
+	return property(idToObj(cls, field), objToID(field), None)
